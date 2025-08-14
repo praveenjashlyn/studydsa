77050c537333fa1114c59efaafdc2a61b7877eac
praveenjashlyn <praveenjashlyn17@gmail.com>
2025-08-14 07:26:45 +0530

    added printer

---
+++ printer.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+
+
+struct teachers{
+	char name[50];
+	char book[50];
+	int pages;
+
+
+}queue[50],teacher[50],tempname;
+int front =-1,rear=-1;
+
+
+
+
+void enqueue(struct teachers teacher){
+	if(front==-1 && rear==-1){
+		front++;rear++;
+		queue[rear]=teacher;
+
+	}
+    else if(rear==10){
+        printf("Queue is full...");
+    }
+    else{
+        queue[++rear]=teacher;
+    }
+
+
+}
+
+
+void main(){
+	int n,sum,i,current;
+    
+	//READ EVERYTHNG..
+	
+	printf("no. of teachers : ");
+	scanf("%d",&n);
+
+
+
+
+
+	for(int i=0;i<n;i++)
+	{	printf("\nTeacher %d \n Enter name, book and pages\n",i+1);
+		scanf(" %[^\n]",teacher[i].name);
+		scanf(" %[^\n]",teacher[i].book);
+		scanf(" %d",&teacher[i].pages);
+        enqueue(teacher[i]);	
+	}
+
+
+    for(int i=0;i<n;i++){
+        printf("pages are %d \n",queue[i].pages);
+    }
+
+
+	printf(" 1.current time : \n 2.waiting time \n 3.Teacher who waited most  \n 4.Exit \n");
+	int choice,time;
+    scanf("%d",&choice);
+
+    while(choice==1 || choice==2 ||choice==3){
+
+    
+        switch(choice){
+            case 1:
+                printf("Enter current time : ");
+                scanf("%d",&time);
+                int calctime=0;
+                int i=0;
+                while(calctime<time)
+                {
+                    calctime=calctime+(queue[i].pages*2);
+                    i++;
+                }
+                printf("name is %s \n",queue[i-1].name);
+                break;
+
+
+            case 2:
+                printf("Enter book name : ");
+                scanf(" %[^\n]",tempname.book);
+                i=0;
+                int waittime=0;
+                while(tempname.book==queue[i].book)
+                {   
+                    waittime+=tempname.pages*2;
+                    i++;
+                }
+                printf("Waiting time of %s is %d \n",tempname.book,waittime);
+                break;
+            
+                
+            case 3:
+                printf("Waited most is %s \n",queue[rear].name);
+                break;
+            
+            default:
+                printf("Thankyou\n");
+            
+
+        }
+        printf(" 1.current time : \n 2.waiting time \n 3.Teacher who waited most  \n 4.Exit \n");
+	    scanf("%d",&choice);
+
+        
+    }
+	
+	
+}
--- train.c
+++ train.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 //queue
 
 struct customers{
@@ -7,7 +8,7 @@ struct customers{
     char name[50];
 }queue[50];
 
-int front,rear,size=10;
+int front,rear,size=10,count=0,i;
 
 void initialise(){
     front=rear=-1;
@@ -19,6 +20,7 @@ void enqueue(){
         printf("Enter Name and tickets count");
         scanf(" %[^\n]%d",queue[rear].name,&queue[rear].ticketsleft);
         printf("Successfully added...");
+        count++;
         
     }
     else if((rear+1)%size==front){
@@ -28,18 +30,84 @@ void enqueue(){
     }
     else{
         rear=(rear+1)%size;
-        queu
+        printf("Enter Name and tickets count");
+        scanf(" %[^\n]%d",queue[rear].name,&queue[rear].ticketsleft);
+        printf("Successfully added...");
+        count++;
+    }
+}
+void dequeue(){
+    if(front==-1 && rear==-1){
+        printf("dequeue not possible");
+    }
+    else if(front==rear){
+        printf("Dequeued person %s ",queue[front].name);
+        front=rear=-1;
+        count--;
+    }
+    else{
+        printf("Dequeued person %s ",queue[front].name);
+        front=(front+1)%size;
+        count--;
+
+    }
+}
+int serve(){
+    if(queue[front].ticketsleft!=0){
+        (queue[front].ticketsleft)--;
+            if((queue[front].ticketsleft)==0){
+                dequeue();
+                return 0;
+            }
+    
+        rear=(rear+1)%size;
+        queue[rear]=queue[front];
+    
+        front=(front+1)%size;
+        return 1;
+    }
+    
+    else{
+        dequeue();
+    }
+
+}
+
+
+void main(){
+
+    initialise();
+    int choice=1;
+    while(choice){
+        printf("What dou you want to do :\n 1.book tickets \n 2. serve ticket \n 3.Current customer served \n 4.Customers Waiting. \n 5.customers list with forms \n");
+        scanf(" %d",&choice);
+        switch(choice){
+            case 1: 
+                enqueue();
+                break;
+            case 2:
+                serve();
+                break;
+            case 3: 
+                printf("%s || %d \n",queue[rear].name,queue[rear].ticketsleft);
+                break;
+            case 4:
+                printf("Customers waiting : %d \n",count);
+                break;
+            case 5:
+            
+                for(i=front;i!=rear;i=(i+1)%size)
+                {   printf("name : %s || forms left :%d \n",queue[i].name,queue[i].ticketsleft);
+                }
+                printf("name : %s || forms left :%d \n",queue[i].name,queue[i].ticketsleft);
+        }
+            
+
     }
 }
 
 
 
-void main(){
-
-    
-    
 
 
-
-}
 
